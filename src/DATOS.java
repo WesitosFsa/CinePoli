bad4bfe3b03548038c877e2bd3a4be55755fe66f
WesitosFsa <137083225+WesitosFsa@users.noreply.github.com>
2024-02-25 10:43:10 -0500

    fixeando errores

--- src/DATOS.java
+++ src/DATOS.java
@@ -7,11 +7,12 @@ public class DATOS extends JFrame{
     private JButton salirButton;
     private JButton reservarAsientoButton;
     private JTextArea textArea1;
+    private JTable table1;
     private JTextArea textArea2;
 
     public DATOS() {
         setContentPane(DATOS);
-        setSize(500,500);
+        setSize(800, 500);
         setResizable(false);
         setLocationRelativeTo(null);
         setUndecorated(true);
@@ -33,17 +34,6 @@ public class DATOS extends JFrame{
         textArea1.append("Le seguirá el \nepisodio 1 del Arco del Entrenamiento de los Pilares, donde veremos el inicio del entrenamiento \nde los Pilares para prepararse de cara a la próxima batalla contra Muzan Kibutsuji.\n");
 
         // Añade el horario de películas al JTextArea2
-        textArea2.append("Horario de Reservas:\n\n");
-        textArea2.append("Lunes:\n");
-        textArea2.append("Sala 1:\n");
-        textArea2.append(" - 12:00 PM: Kimetsu no Yaiba - To the Hashira Training - ESPAÑOL\n");
-        textArea2.append(" - 3:00 PM: Kimetsu no Yaiba - To the Hashira Training - JAPONES\n");
-        textArea2.append(" - 6:00 PM: Kimetsu no Yaiba - To the Hashira Training - INGLES\n");
-        textArea2.append("\nMartes:\n");
-        textArea2.append("\nSala 2:\n");
-        textArea2.append(" - 1:00 PM: Kimetsu no Yaiba - To the Hashira Training - ESPAÑOL\n");
-        textArea2.append(" - 4:00 PM: Kimetsu no Yaiba - To the Hashira Training - JAPONES\n");
-        textArea2.append(" - 7:00 PM: Kimetsu no Yaiba - To the Hashira Training - INGLES\n");
         salirButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
