7b9301aaa4bac62cf9201739647d7208763f6428
IGNN3LZ3R0 <ignnel01end@gmail.com>
2024-03-02 18:35:43 -0500

    conexion a la base de datos de la pantalla verpelis

--- src/verpelis.java
+++ src/verpelis.java
@@ -2,36 +2,64 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.prefs.Preferences;
 
-public class verpelis extends JFrame{
-    JPanel panel1;
+public class verpelis extends JFrame {
+    private JPanel panel1;
     private JButton añadirButton;
     private JButton eliminarButton;
     private JTable peliculasTable;
     private JButton menuPrincipalButton;
     private DefaultTableModel tableModel;
-    List<Pelicula> listaPeliculas;
-
-    private verpelis salasScreen;
-    private ADMIN adminScreen;
+    private List<Pelicula> listaPeliculas;
+    private Connection conexion;
 
     private Preferences preferences = Preferences.userNodeForPackage(getClass());
 
-    // Constructor de la clase verpelis
     public verpelis() {
-        /*Configuracion dela pantalla VerPelis*/
+        // Configuración de la ventana
         setContentPane(panel1);
-        setSize(800,500);
+        setSize(800, 500);
         setResizable(false);
         setLocationRelativeTo(null);
         setUndecorated(true);
         setVisible(true);
+
+        // Inicialización de la lista de películas
         this.listaPeliculas = new ArrayList<>();
 
+        // Establecer la conexión a la base de datos
+        establecerConexion();
+
         // Configuración del modelo de tabla para mostrar películas
+        configurarModeloTabla();
+
+        // Configuración de ActionListener para los botones
+        añadirButton.addActionListener(e -> agregarPelicula());
+        eliminarButton.addActionListener(e -> eliminarPelicula());
+        menuPrincipalButton.addActionListener(e -> volverMenuPrincipal());
+
+        // Cargar las películas desde la base de datos al inicializar
+        cargarPeliculasDesdeBaseDatos();
+    }
+
+    private void establecerConexion() {
+        try {
+            Main conexionbd = new Main();
+            conexion = conexionbd.establecerConexion();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error al conectar con la base de datos: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void configurarModeloTabla() {
         tableModel = new DefaultTableModel();
         tableModel.addColumn("ID");
         tableModel.addColumn("Nombre");
@@ -39,174 +67,122 @@ public class verpelis extends JFrame{
         tableModel.addColumn("Descripción");
         tableModel.addColumn("Director");
         tableModel.addColumn("Año");
-        tableModel.addColumn("Clasificacion");
-        tableModel.addColumn("Ruta imaguen");
-
+        tableModel.addColumn("Clasificación");
+        tableModel.addColumn("Ruta imagen");
         peliculasTable.setModel(tableModel);
         peliculasTable.setEnabled(false);
-
-        // Configuración de ActionListener para el botón "Añadir"
-        añadirButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                agregarPelicula();
-            }
-        });
-
-        // Configuración de ActionListener para el botón "Eliminar"
-        eliminarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                eliminarPelicula();
-            }
-        });
-
-        // Configuración de ActionListener para el botón "Menu Principal"
-        menuPrincipalButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ADMIN Admin = new ADMIN();
-                Admin.setVisible(true);
-                JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(panel1);
-                frame.dispose();
-            }
-        });
-
-        // Mostrar las películas y cargarlas desde preferencias al inicializar
-        mostrarPeliculas();
-        cargarPeliculasDesdePreferencias();
     }
 
-    // Método para agregar una nueva película
     private void agregarPelicula() {
-        // Solicitar al usuario ingresar información de la nueva película
-        String nombre = JOptionPane.showInputDialog("Ingrese el nombre de la película:");
-        String genero = JOptionPane.showInputDialog("Ingrese el género de la película:");
-        String descripcion = JOptionPane.showInputDialog("Ingrese la descripción de la película:");
-        String director = JOptionPane.showInputDialog("Ingrese la director de la película:");
-        int anio = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el año de la película:"));
-        String clasificacion = JOptionPane.showInputDialog("Ingrese la clasificacion de la película:");
-        String imaguen = JOptionPane.showInputDialog("Ingrese la ruta de la imaguen de la película:");
+        try {
+            String nombre = JOptionPane.showInputDialog("Ingrese el nombre de la película:");
+            String genero = JOptionPane.showInputDialog("Ingrese el género de la película:");
+            String descripcion = JOptionPane.showInputDialog("Ingrese la descripción de la película:");
+            String director = JOptionPane.showInputDialog("Ingrese la director de la película:");
+            int anio = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el año de la película:"));
+            String clasificacion = JOptionPane.showInputDialog("Ingrese la clasificación de la película:");
+            String imagen = JOptionPane.showInputDialog("Ingrese la ruta de la imagen de la película:");
 
-        // Generar un nuevo ID para la película
-        int id = listaPeliculas.size() + 1;
-        Pelicula nuevaPelicula = new Pelicula(id, nombre, genero, descripcion, director, anio, clasificacion, imaguen);
-        listaPeliculas.add(nuevaPelicula);
+            // Insertar la película en la base de datos
+            insertarPeliculaEnBaseDatos(nombre, genero, descripcion, director, anio, clasificacion, imagen);
 
-        // Guardar películas en preferencias
-        guardarPeliculasEnPreferencias();
-
-        // Actualizar la tabla de películas y enviar la información a la pantalla salas
-        mostrarPeliculas();
-        if (salasScreen != null) {
-            salasScreen.actualizarListaPeliculas(listaPeliculas);
+            // Actualizar la tabla con las películas de la base de datos
+            cargarPeliculasDesdeBaseDatos();
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "El año debe ser un número entero.", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
-    // Método para eliminar una película existente
+    private void insertarPeliculaEnBaseDatos(String nombre, String genero, String descripcion, String director, int anio, String clasificacion, String imagen) {
+        try {
+            String sql = "INSERT INTO peliculas (nombre_pelicula, genero, sinopsis, Director, anho, clasificacion, foto_pelicula) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement statement = conexion.prepareStatement(sql);
+            statement.setString(1, nombre);
+            statement.setString(2, genero);
+            statement.setString(3, descripcion);
+            statement.setString(4, director);
+            statement.setInt(5, anio);
+            statement.setString(6, clasificacion);
+            statement.setString(7, imagen);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error al insertar la película en la base de datos: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
     private void eliminarPelicula() {
         try {
-            // Solicitar al usuario ingresar el ID de la película a eliminar
             int idEliminar = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID de la película a eliminar:"));
-            // Intentar remover la película de la lista
-            boolean removed = listaPeliculas.removeIf(pelicula -> pelicula.getId() == idEliminar);
-
-            // Guardar películas en preferencias
-            guardarPeliculasEnPreferencias();
-
-            // Actualizar la tabla de películas
-            mostrarPeliculas();
-
-            // Mostrar mensaje si la película no existe
-            if (!removed) {
-                JOptionPane.showMessageDialog(null, "La película con ID " + idEliminar + " no existe.");
-            }
+            // Eliminar la película de la base de datos
+            eliminarPeliculaEnBaseDatos(idEliminar);
+            // Actualizar la tabla con las películas de la base de datos
+            cargarPeliculasDesdeBaseDatos();
         } catch (NumberFormatException ex) {
-            JOptionPane.showMessageDialog(null, "Ingrese un ID válido (número entero).");
+            JOptionPane.showMessageDialog(null, "Ingrese un ID válido (número entero).", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
-    // Método para actualizar la tabla de películas en la interfaz gráfica
-    void mostrarPeliculas() {
-        tableModel.setRowCount(0);
+    private void eliminarPeliculaEnBaseDatos(int idEliminar) {
+        try {
+            String sql = "DELETE FROM peliculas WHERE id_pelicula = ?";
+            PreparedStatement statement = conexion.prepareStatement(sql);
+            statement.setInt(1, idEliminar);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error al eliminar la película de la base de datos: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 
+
+    private void cargarPeliculasDesdeBaseDatos() {
+        try {
+            String sql = "SELECT * FROM peliculas";
+            PreparedStatement statement = conexion.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+
+            listaPeliculas.clear(); // Limpiar la lista antes de cargar las películas desde la base de datos
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id_pelicula");
+                String nombre = resultSet.getString("nombre_pelicula");
+                String genero = resultSet.getString("genero");
+                String descripcion = resultSet.getString("sinopsis");
+                String director = resultSet.getString("Director");
+                int anio = resultSet.getInt("anho");
+                String clasificacion = resultSet.getString("clasificacion");
+                String imagen = resultSet.getString("foto_pelicula");
+
+                Pelicula pelicula = new Pelicula(id, nombre, genero, descripcion, director, anio, clasificacion, imagen);
+                listaPeliculas.add(pelicula);
+            }
+
+            mostrarPeliculas(); // Actualizar la tabla con las películas cargadas desde la base de datos
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error al cargar las películas desde la base de datos: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+
+    private void mostrarPeliculas() {
+        // Limpiar el modelo de tabla
+        tableModel.setRowCount(0);
+        // Agregar las películas al modelo de tabla
         for (Pelicula pelicula : listaPeliculas) {
-            Object[] rowData = {pelicula.getId(), pelicula.getNombre(), pelicula.getGenero(), pelicula.getDescripcion(), pelicula.getDirector(), pelicula.getAnio(), pelicula.getClasificacion(), pelicula.getImaguen()};
+            Object[] rowData = {pelicula.getId(), pelicula.getNombre(), pelicula.getGenero(), pelicula.getDescripcion(), pelicula.getDirector(), pelicula.getAnio(), pelicula.getClasificacion(), pelicula.getImagen()};
             tableModel.addRow(rowData);
         }
     }
-    // Método para cargar películas desde preferencias al iniciar la aplicación
-    private void cargarPeliculasDesdePreferencias() {
-        String peliculasGuardadas = preferences.get("peliculas", "");
-        if (!peliculasGuardadas.isEmpty()) {
-            // Parsear la cadena y cargar las películas en la lista
-            String[] peliculasArray = peliculasGuardadas.split(";");
-            listaPeliculas.clear(); // Limpiar la lista antes de cargar las películas
-            for (String peliculaStr : peliculasArray) {
-                String[] datos = peliculaStr.split(",");
-                int id = Integer.parseInt(datos[0]);
-                String nombre = datos[1];
-                String genero = datos[2];
-                String descripcion = datos[3];
-                String director = datos[4];
-                int anio = Integer.parseInt(datos[5]);
-                String clasificacion = datos[6];
-                String imaguen = datos[7];
-                Pelicula pelicula = new Pelicula(id, nombre, genero, descripcion, director, anio, clasificacion, imaguen);
-                listaPeliculas.add(pelicula);
-            }
-            // Actualizar la pantalla con las películas cargadas
-            mostrarPeliculas();
 
-            // También puedes actualizar la lista en la pantalla de salas si es necesario
-            if (salasScreen != null) {
-                salasScreen.actualizarListaPeliculas(listaPeliculas);
-            }
-        }
+    private void volverMenuPrincipal() {
+        ADMIN Admin = new ADMIN();
+        Admin.setVisible(true);
+        JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(panel1);
+        frame.dispose();
     }
 
-    // Método para guardar películas en preferencias al realizar cambios
-    private void guardarPeliculasEnPreferencias() {
-        StringBuilder peliculasStr = new StringBuilder();
-        for (Pelicula pelicula : listaPeliculas) {
-            peliculasStr.append(pelicula.getId()).append(",")
-                    .append(pelicula.getNombre()).append(",")
-                    .append(pelicula.getGenero()).append(",")
-                    .append(pelicula.getDescripcion()).append(";")
-                    .append(pelicula.getDirector()).append(";")
-                    .append(pelicula.getAnio()).append(";")
-                    .append(pelicula.getClasificacion()).append(";")
-                    .append(pelicula.getImaguen()).append(";");
-        }
-        preferences.put("peliculas", peliculasStr.toString());
-    }
-
-    // Método para mostrar la pantalla principal de "VerPelis"
-    public void mostrarVerPelis() {
-        JFrame frame = new JFrame("VerPelis");
-        frame.setContentPane(panel1);  // Utiliza el panel1 actual de la instancia existente
-        frame.setSize(800, 500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Actualiza la lista de películas al volver a la pantalla
-        mostrarPeliculas();
-
-        // Envía la información actualizada de la película a la pantalla salas
-        if (salasScreen != null) {
-            salasScreen.actualizarListaPeliculas(listaPeliculas);
-        }
-
-        frame.setVisible(true);
-    }
-
-    // Método principal para ejecutar la aplicación
-
-    // Método para actualizar la lista de películas desde otras partes de la aplicación
-    public void actualizarListaPeliculas(List<Pelicula> listaPeliculas) {
-        this.listaPeliculas = listaPeliculas;
-        mostrarPeliculas();
-    }
-    // Clase estática interna que representa una película
     public static class Pelicula {
         private int id;
         private String nombre;
@@ -215,11 +191,9 @@ public class verpelis extends JFrame{
         private String director;
         private int anio;
         private String clasificacion;
-        private String imaguen;
+        private String imagen;
 
-
-        // Constructor de la clase Pelicula
-        public Pelicula(int id, String nombre, String genero, String descripcion, String director, int anio, String clasificacion, String imaguen) {
+        public Pelicula(int id, String nombre, String genero, String descripcion, String director, int anio, String clasificacion, String imagen) {
             this.id = id;
             this.nombre = nombre;
             this.genero = genero;
@@ -227,10 +201,9 @@ public class verpelis extends JFrame{
             this.director = director;
             this.anio = anio;
             this.clasificacion = clasificacion;
-            this.imaguen = imaguen;
+            this.imagen = imagen;
         }
 
-        // Métodos getter para obtener información de la película
         public int getId() {
             return id;
         }
@@ -246,20 +219,21 @@ public class verpelis extends JFrame{
         public String getDescripcion() {
             return descripcion;
         }
-        public String getDirector() { return director; }
+
+        public String getDirector() {
+            return director;
+        }
+
         public int getAnio() {
             return anio;
         }
+
         public String getClasificacion() {
             return clasificacion;
         }
-        public String getImaguen() {
-            return imaguen;
-        }
 
+        public String getImagen() {
+            return imagen;
+        }
     }
 }
-
-
-
-
